c4dcfeeee39e586ce7cfa31ed02d81a4773f1840
Juan Manuel <juanmanuel@jmchaparro.com>
2021-11-21 20:14:22 -0500

    Trabajo C4-Sprint 2
    Primera version

--- src.js
+++ src.js
@@ -1,80 +1,97 @@
-/*  Calcular los porcentajes de concentración de cada gas
-    Recibe los límites superiores de cada gas (100%) a ser analizado y los valores reportados de los mismos
-    Retorna un objeto con el porcentaje que indica el nivel de concentración de cada gas
-*/
-const calcularPocentajes = (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2) => {
-    let datosPorGas = [[limiteCO,valorCO,'porcentajeCO'],[limiteCO2,valorCO2,'porcentajeCO2'],[limiteHC,valorHC,'porcentajeHC'],[limiteO2,valorO2,'porcentajeO2']];
-    let nivelEmisiones = {};
+var objeto = {};
 
-    for(let i=0; i<datosPorGas.length; i++){
-        let porcentaje = datosPorGas[i][0]===0?0:datosPorGas[i][1]/(datosPorGas[i][0]/100);
-        Object.defineProperty(nivelEmisiones,datosPorGas[i][2],{value:porcentaje,writable:true,enumerable: true,
-            configurable: true}); 
-    }
-    return nivelEmisiones;
-}
+const calcularPocentajes = (
+  limiteCO,
+  limiteCO2,
+  limiteHC,
+  limiteO2,
+  valorCO,
+  valorCO2,
+  valorHC,
+  valorO2
+) => {
+  if (limiteCO == 0) {
+    objeto.porcentajeCO = 0;
+  } else {
+    let porcentajeCO = (valorCO * 100) / limiteCO;
+    objeto.porcentajeCO = porcentajeCO;
+  }
 
-const registrarCO = (value) => {
-    if (value >= rangosCO[0].de && value <= rangosCO[0].hasta) {
-        console.log(rangosCO[0].etiqueta)
-        return rangosCO[0].etiqueta;
-    }
-    if (value >= rangosCO[1].de && value <= rangosCO[1].hasta) {
-        console.log(rangosCO[1].etiqueta)
-        return rangosCO[1].etiqueta;
-    }
-    if (value > 15 || value < 0) {
-        console.log("fuera_de_rango")
-        return "fuera_de_rango";
-    }
+  if (limiteCO2 == 0) {
+    objeto.porcentajeCO2 = 0;
+  } else {
+    let porcentajeCO2 = (valorCO2 * 100) / limiteCO2;
+    objeto.porcentajeCO2 = porcentajeCO2;
+  }
 
-}
-const registrarCO2 = (value) => {
-    if (value >= rangosCO2[0].de && value <= rangosCO2[0].hasta) {
-        console.log(rangosCO2[0].etiqueta)
-        return rangosCO2[0].etiqueta;
-    }
-    if (value >= rangosCO2[1].de && value <= rangosCO2[1].hasta) {
-        console.log(rangosCO2[1].etiqueta)
-        return rangosCO2[1].etiqueta;
-    }
-    if (value > 30 || value < 0) {
-        console.log("fuera_de_rango")
-        return "fuera_de_rango";
-    }
+  if (limiteHC == 0) {
+    objeto.porcentajeHC = 0;
+  } else {
+    let porcentajeHC = (valorHC * 100) / limiteHC;
+    objeto.porcentajeHC = porcentajeHC;
+  }
 
-}
-const registrarHC = (value) => {
-    if (value >= rangosHC[0].de && value <= rangosHC[0].hasta) {
-        console.log(rangosHC[0].etiqueta)
-        return rangosHC[0].etiqueta;
-    }
-    if (value >= rangosHC[1].de && value <= rangosHC[1].hasta) {
-        console.log(rangosHC[1].etiqueta)
-        return rangosHC[1].etiqueta;
-    }
-    if (value > 11000 || value < 0) {
-        console.log("fuera_de_rango")
-        return "fuera_de_rango";
-    }
+  if (limiteO2 == 0) {
+    objeto.porcentajeO2 = 0;
+  } else {
+    let porcentajeO2 = (valorO2 * 100) / limiteO2;
+    objeto.porcentajeO2 = porcentajeO2;
+  }
 
-}
-const registrarO2 = (value) => {
-    if (value >= rangosO2[0].de && value <= rangosO2[0].hasta) {
-        console.log(rangosO2[0].etiqueta)
-        return rangosO2[0].etiqueta;
-    }
-    if (value >= rangosO2[1].de && value <= rangosO2[1].hasta) {
-        console.log(rangosO2[1].etiqueta)
-        return rangosO2[1].etiqueta;
-    }
-    if (value > 30 || value < 0) {
-        console.log("fuera_de_rango")
-        return "fuera_de_rango";
-    }
-}
+  return objeto;
+};
+
+const registrarCO = (valorCO) => {
+  if (valorCO < 0) {
+    return "fuera_de_rango";
+  } else {
+    const etiqueta =
+      valorCO >= rangosCO[0].de && valorCO <= rangosCO[0].hasta
+        ? "Parametro CO en rango estandar"
+        : "Parametro CO fuera de rango";
+    return etiqueta;
+  }
+};
+
+const registrarCO2 = (valorCO2) => {
+  if (valorCO2 < 0) {
+    return "fuera_de_rango";
+  } else {
+    const etiqueta =
+      valorCO2 >= rangosCO2[0].de && valorCO2 <= rangosCO2[0].hasta
+        ? "Parametro CO2 en rango estandar"
+        : "Parametro CO2 fuera de rango";
+    return etiqueta;
+  }
+};
+
+const registrarHC = (valorHC) => {
+  if (valorHC < 0) {
+    return "fuera_de_rango";
+  } else {
+    const etiqueta =
+      valorHC >= rangosHC[0].de && valorHC <= rangosHC[0].hasta
+        ? "Parametro HC en rango estandar"
+        : "Parametro HC fuera de rango";
+    return etiqueta;
+  }
+};
+
+const registrarO2 = (valorO2) => {
+  if (valorO2 < 0) {
+    return "fuera_de_rango";
+  } else {
+    const etiqueta =
+      valorO2 >= rangosO2[0].de && valorO2 <= rangosO2[0].hasta
+        ? "Parametro O2 en rango estandar"
+        : "Parametro O2 fuera de rango";
+    return etiqueta;
+  }
+};
+
 module.exports.registrarCO = registrarCO;
 module.exports.registrarCO2 = registrarCO2;
 module.exports.registrarHC = registrarHC;
 module.exports.registrarO2 = registrarO2;
+
 module.exports.calcularPocentajes = calcularPocentajes;
